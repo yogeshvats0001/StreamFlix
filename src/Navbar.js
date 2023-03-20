5e7fe92981164ce67294b440631ab1904fa85b1d
Yogesh Vats <yogisharma1062@gmail.com>
2023-03-21 04:52:06 +0530

    Working on Navbar

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Navbar.css";
 import logo from "./Resources/logo.png";
+import avatar from "./Resources/avatar.png";
 
 function Navbar() {
   return (
     <div className="nav">
       <img className="nav__logo" src={logo} alt="StreamFlix Logo" />
+      <img className="nav__avatar" src={avatar} alt="StreamFlix Logo" />
     </div>
   );
 }
