7ee25e0b0c0d1df564d15f4b3c2742a4c3c71d38
Yogesh Vats <yogisharma1062@gmail.com>
2023-03-22 01:05:06 +0530

    Finalizing Navbar Component

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Navbar.css";
 import logo from "./Resources/logo.png";
 import avatar from "./Resources/avatar.png";
 
 function Navbar() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 100) {
+        setShow(true);
+      } else setShow(false);
+    });
+  }, []);
   return (
-    <div className="nav">
+    <div className={`nav ${show && "nav__black"}`}>
       <img className="nav__logo" src={logo} alt="StreamFlix Logo" />
       <img className="nav__avatar" src={avatar} alt="StreamFlix Logo" />
     </div>
